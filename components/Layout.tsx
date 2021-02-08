ddb37f85964dc7cbb9fd9b87871b7967b7d9d708
KohnoTaiyo <taiyowork66@gmail.com>
2021-02-08 23:59:12 +0900

    add css framework tailwind

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,8 +7,8 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+const Layout = ({ children, title = 'Souvenir' }: Props) => (
+  <>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
@@ -35,7 +35,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <hr />
       <span>I'm here to stay (Footer)</span>
     </footer>
-  </div>
+  </>
 )
 
 export default Layout
