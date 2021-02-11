88e7b74afe8c65fd84e029a85fbc2bd790592d2f
KohnoTaiyo <taiyowork66@gmail.com>
2021-02-11 12:48:13 +0900

    add tailwindcss

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from 'react'
-import Link from 'next/link'
+// import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
+import 'tailwindcss/tailwind.css'
 
 type Props = {
   children?: ReactNode
@@ -14,7 +16,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
+    {/* <header>
       <nav>
         <Link href="/">
           <a>Home</a>
@@ -29,7 +31,8 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         </Link>{' '}
         | <a href="/api/users">Users API</a>
       </nav>
-    </header>
+    </header> */}
+    <Image src="/img/hero.png" width="500" height="340" layout={'responsive'} />
     {children}
     <footer>
       <hr />
