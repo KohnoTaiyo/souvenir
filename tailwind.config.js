4e5266d406a2af3f886fe21a79f80d7d9f86fa0e
KohnoTaiyo <taiyowork66@gmail.com>
2021-02-24 00:50:16 +0900

    modify congig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
     extend: {
