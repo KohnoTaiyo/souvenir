1baeb44d1c00dd094cb9cec407dc3acbc8e1a76f
KohnoTaiyo <taiyowork66@gmail.com>
2021-03-14 15:10:29 +0900

    key test

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -1,12 +1,14 @@
 const sgMail = require('@sendgrid/mail')
 
-export default async function (req, res) {
+export default async function (req) {
+  console.log('key before')
   sgMail.setApiKey(process.env.NEXT_PUBLIC_MAIL_KEY)
+  console.log('key ok')
 
   const { message } = req.body
 
   const content = {
-    to: 'taiyowork66@gmail.com',
+    to: 'souvenir2020jp@gmail.com',
     from: 'taiyowork66@gmail.com',
     subject: `HPから${message.inquire}が届きました`,
     html: `
@@ -17,8 +19,10 @@ export default async function (req, res) {
     <br>
     <p>${message.text}</p>`,
   }
+  console.log('before send')
   sgMail
     .send(content)
     .then(() => console.log(content))
     .catch(() => console.log('error'))
+  console.log('finish')
 }
