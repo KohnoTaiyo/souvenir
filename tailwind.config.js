acd52e6653d45af6e2300f09c618d20da210d064
uchidayuuki <uki0324@gmail.com>
2022-02-28 22:13:30 +0900

    vercelで正常に反映されなかったのでpurgeの値を削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    // './src/components/**/*.{js,ts,jsx,tsx}',
-    // './src/pages/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: [],
   darkMode: false,
   theme: {
     minHeight: {
