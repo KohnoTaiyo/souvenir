cff1ac6e1f3b221b7e5c2f0ab6c0c1dc9dee9cda
KohnoTaiyo <59274850+KohnoTaiyo@users.noreply.github.com>
2023-06-04 12:38:34 +0900

    fix

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -4,25 +4,21 @@ import { Resend } from 'resend'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const resend = new Resend(process.env.NEXT_PUBLIC_MAIL_KEY)
-    if (resend) {
-      const { message } = req.body
-      const data = await resend.sendEmail({
-        from: 'onboarding@resend.dev',
-        to: 'taiyowork66@gmail.com',
-        subject: `HPから${message.inquire}が届きました`,
-        html: `
-        <p>内容：${message.inquire}</p>
-        <p>名前：${message.name}</p>
-        <p>フリガナ：${message.kana}</p>
-        <p>メール：${message.email}</p>
-        <br>
-        <p>${message.text.replace(/\n/g, '<br />')}</p>
-        `,
-      })
-      res.status(200).json(data)
-    } else {
-      console.log('aaa')
-    }
+    const { message } = req.body
+    const data = await resend.sendEmail({
+      from: 'onboarding@resend.dev',
+      to: 'taiyowork66@gmail.com',
+      subject: `HPから${message.inquire}が届きました`,
+      html: `
+      <p>内容：${message.inquire}</p>
+      <p>名前：${message.name}</p>
+      <p>フリガナ：${message.kana}</p>
+      <p>メール：${message.email}</p>
+      <br>
+      <p>${message.text.replace(/\n/g, '<br />')}</p>
+      `,
+    })
+    res.status(200).json(data)
   } catch (error) {
     console.error(error)
     res.status(400).json(error)
